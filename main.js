dfbfeb8c337a71193c00cee9a7d0196e2c6683fa
Y. Kotvizky <ekotvizky@gmail.com>
2025-02-03 09:48:23 +0200

    start with mobile

---
+++ main.js
@@ -0,0 +1,25 @@
+const div = document.querySelector('.header h1')
+const services = document.querySelectorAll('.service')
+
+div.insertAdjacentHTML('beforeend',
+    `<br><span style='font-size: 0.7rem'>resolution: ${window.innerWidth} ${window.innerHeight} </span>` 
+)
+
+let serviceInex = 0
+const lastServiceNumber = services.length - 1
+
+
+const showService = () => {
+    serviceInex = (serviceInex === lastServiceNumber) ? 0 : serviceInex + 1
+    services.forEach( (currentService, currentIndex) => {
+        if (currentIndex === serviceInex) 
+            currentService.style.opacity = 1
+        else 
+        currentService.style.opacity = 0
+    }
+
+    )
+    //console.log(serviceInex)
+}
+
+//setInterval(showService, 3000)
