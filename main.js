88078c3fe9408715ad788929310feeffcd63a8ca
Yevgen Kotvitskyi <yevgeniy.kotvitskyi@gmail.com>
2025-02-04 00:29:37 +0200

    start with opasity transiion

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
-const div = document.querySelector('.header h1')
-const services = document.querySelectorAll('.service')
+const div = document.querySelector(".footer span");
+const services = document.querySelectorAll(".service");
 
-div.insertAdjacentHTML('beforeend',
-    `<br><span style='font-size: 0.7rem'>resolution: ${window.innerWidth} ${window.innerHeight} </span>` 
-)
+div.insertAdjacentHTML(
+  "beforeend",
+  `<span style='font-size: 0.7rem'>resolution: ${window.innerWidth} ${window.innerHeight} </span>`
+);
 
-let serviceInex = 0
-const lastServiceNumber = services.length - 1
-
+let serviceInex = 0;
+const lastServiceNumber = services.length - 1;
 
 const showService = () => {
-    serviceInex = (serviceInex === lastServiceNumber) ? 0 : serviceInex + 1
-    services.forEach( (currentService, currentIndex) => {
-        if (currentIndex === serviceInex) 
-            currentService.style.opacity = 1
-        else 
-        currentService.style.opacity = 0
-    }
+  serviceInex = serviceInex === lastServiceNumber ? 0 : serviceInex + 1;
+  services.forEach((currentService, currentIndex) => {
+    if (currentIndex === serviceInex) currentService.style.opacity = 1;
+    else currentService.style.opacity = 0;
+  });
+};
 
-    )
+function isTouchDevice() {
+  return (
+    "ontouchstart" in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
 }
 
-setInterval(showService, 3000)
+let interval = null;
+
+if (isTouchDevice()) {
+  interval = setInterval(showService, 3000);
+}
+
+addEventListener("resize", (e) => {
+  if (!isTouchDevice()) {
+    clearInterval(interval);
+
+    services.forEach((currentService, currentIndex) => {
+      currentService.style.opacity = 1;
+    });
+  } else {
+    if (!interval) {
+      interval = setInterval(showService, 3000);
+    }
+  }
+});
